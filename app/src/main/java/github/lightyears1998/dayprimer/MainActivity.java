1391be37b0a391016fe6a8f5322af09906fc12c0
lightyears1998 <lightyears1998@hotmail.com>
2021-06-17 12:59:06 +0800

    feat: 初始化提交
    白茫茫一片真干净！

---
+++ app/src/main/java/github/lightyears1998/dayprimer/MainActivity.java
@@ -0,0 +1,14 @@
+package github.lightyears1998.dayprimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
